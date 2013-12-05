c359b42db9f6e7851d7ba6e52e5c304816352c29
Carlos Herrera <carlos.h.ficachi@gmail.com>
2013-12-04 20:45:12 -0800

    COPY MOVE REMOVE

---
+++ copy.c
@@ -0,0 +1,36 @@
+int main(int argc, char *argv[])
+{
+char buf;
+int source, target, n;
+if(argc!=3)
+{
+printf("Error");
+}
+else
+{
+source = open(argv[1],O_RDONLY);
+if(source == -1)
+{
+printf("Error");
+}
+else
+{
+target = open(argv[2],O_WRONLY|O_CREAT);
+if(target ==-1)
+{
+printf("Error");
+}
+else
+{
+while((n=read(source,&buf,1))!=0)
+{
+write(target,&buf,1);
+}
+write(source,"Copiado\n",15);
+close(source);
+close(target);
+}
+}
+}
+return 0;
+}
